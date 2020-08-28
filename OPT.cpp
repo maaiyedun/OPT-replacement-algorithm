0539e069d53040c535195e0137f4f3a0228989a2
maaiyedun <44991916+maaiyedun@users.noreply.github.com>
2020-08-28 00:06:43 -0500

    Update OPT.cpp

--- OPT.cpp
+++ OPT.cpp
@@ -1,6 +1,10 @@
+ // LRU page replacement VS.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
+
 #include <map>
 #include <string.h>
 #include <fstream>
@@ -8,118 +12,96 @@
 #include <math.h>
 #include <stdio.h>
 
+
 using namespace std;
+
+
 int main() {
-	int cSize=0;
-	int pSize=0;
-	int i=0;
-	int j=0;
-	int k=0;
-	int con=0;
+	int n= 0;
+	int i= 0;
+	int j= 0;
+	int k= 0;
+	int hit= 0;
+	int con = 0;
+	int m=0;
 	string line;
 	cout << "Enter the cache Size" << endl;
-	cin >> cSize;
+	cin >> n;
 
-	ifstream myfile("cs.trc");
-	while (std::getline(myfile, line)) {
-		++pSize;
+
+
+	ifstream myfile("gli.trc");
+
+	while (getline(myfile, line)) {
+
+		m++;
 	}
-	myfile.close();
+
+		myfile.close();
 
 
-vector<int> p(pSize);
+	vector<int> p(m);
+	vector<int> hi(m);
 
-ifstream Cfile("cs.trc");
-
-	for (i = 0; i < pSize; i++) {
-    while (std::getline(Cfile, line)) {
-
+	ifstream Cfile("gli.trc");
+	if (Cfile.is_open()) {
+	for (i = 0; i < m; i++) {
+		getline(Cfile, line);
 		stringstream geek(line);
 		geek>>con;
+
 		 p[i]=con;
-        }
+		}
+		Cfile.close();
 	}
-	Cfile.close();
 
-
-
-vector<vector<int>> a(cSize, vector<int>(pSize, -1));
-map <int, int> mp;
-for (i = 0; i < pSize; i++) {
-	vector<int> op;
-	vector<pair<int, int>> c;
-	for (auto q : mp) {
-		c.push_back({ q.second,q.first });
+	vector<vector<int>> a(n);
+	for (i = 0; i < n; i++) {
+		a[i] = vector<int>(m, -1);
 	}
-	for (int q = i + 1; q < pSize; q++) {
-		for (j = 0; j < cSize; j++) {
-			if (a[j][i] == p[q]) {
-				op.push_back(p[q]);
+	map <int, int> mp;
+	for (i = 0; i < m; i++) {
+		vector<pair<int, int>> c;
+		for (auto q : mp) {
+			c.push_back({ q.second,q.first });
+		}
+		sort(c.begin(), c.end());
+		bool hasrun = false;
+		for (j = 0; j < n; j++) {
+			if (a[j][i] == p[i]) {
+				hit++;
+				hi[i] = 1;
+				mp[p[i]] = 1;
+				hasrun = true;
+				break;
+			}
+			if (a[j][i] == -1) {
+				for (k = i; k < m; k++)
+					a[j][k] = p[i];
+				mp[p[i]]++;
+				hasrun = true;
+				break;
 			}
 		}
-	}
-	sort(op.begin(), op.end());
-	op.erase(unique(op.begin(), op.end()), op.end());
-	bool dontCall = true;
-	if (op.size() == cSize) {
-		dontCall = false;
-	}
-	sort(c.begin(), c.end());
-	bool hasrun = false;
-	for (j = 0; j < cSize; j++) {
-		if (a[j][i] == p[i]) {
-			mp[p[i]]++;
-			hasrun = true;
-			break;
-		}
-		if (a[j][i] == -1) {
-			for (k = i; k < pSize; k++)
-				a[j][k] = p[i];
-			mp[p[i]]++;
-			hasrun = true;
-			break;
-		}
-	}
-	if (j == cSize || hasrun == false) {
-		for (j = 0; j < cSize; j++) {
-			if (dontCall == true) {
+		if (j == n || hasrun == false) {
+			for (j = 0; j < n; j++) {
 				if (a[j][i] == c[c.size() - 1].second) {
 					mp.erase(a[j][i]);
-					for (k = i; k < pSize; k++)
-						a[j][k] = p[i];
-					mp[p[i]]++;
-					break;
-				}
-			}
-			else if (dontCall == false) {
-				if (a[j][i] == op[op.size() - 1]) {
-					mp.erase(a[j][i]);
-					for (k = i; k < pSize; k++)
+					for (k = i; k < m; k++)
 						a[j][k] = p[i];
 					mp[p[i]]++;
 					break;
 				}
 			}
 		}
-	}
-	for (auto q : mp) {
-		if (q.first != p[i]) {
-			mp[q.first]++;
+		for (auto q : mp) {
+			if (q.first != p[i]) {
+				mp[q.first]++;
+			}
 		}
 	}
-}
-int hit = 0;
-vector<int> hitv(pSize);
-for (i = 1; i < pSize; i++) {
-	for (j = 0; j < cSize; j++) {
-		if (p[i] == a[j][i - 1]) {
-			hit++;
-			hitv[i] = 1;
-			break;
-		}
-	}
+	cout << "Page Hit " << hit << endl << "Page Fault " << m - hit << endl;
+	return 0;
 }
 
-cout << "Hit " << hit << endl << "Page Fault " << pSize - hit << endl;
-return 0;
-}
+
